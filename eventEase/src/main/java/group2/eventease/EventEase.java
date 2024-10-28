79efa1957a81c13eeabc48d29753fd5f63cf4a78
Utsho11 <utshoroy521@gmail.com>
2024-10-28 20:04:52 +0600

    bug fixed

---
+++ eventEase/src/main/java/group2/eventease/EventEase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package group2.eventease;
+
+/**
+ *
+ * @author utsho
+ */
+public class EventEase {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- src/main/java/group2/eventease/EventEase.java
+++
@@ -1,8 +0,0 @@
-package group2.eventease;
-
-public class EventEase {
-
-    public static void main(String[] args) {
-        System.out.println("The Project is coming soon...");
-    }
-}
